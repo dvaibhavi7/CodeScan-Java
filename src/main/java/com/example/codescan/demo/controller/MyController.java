8eeab9f10fdf428651ec04b588c5f5ff601869c1
vhdeshpande <vaibhavihemantdeshpande@gmail.com>
2023-03-05 23:58:11 -0500

    US1001 - Adding my controller

---
+++ src/main/java/com/example/codescan/demo/controller/MyController.java
@@ -0,0 +1,26 @@
+@RestController
+public class MyController {
+    
+    private final MyService service;
+    
+    public MyController(MyService service) {
+        this.service = service;
+    }
+    
+    @GetMapping("/my-endpoint")
+    public String myEndpoint() {
+        String message = service.getMessage();
+        return "Received message: " + message;
+    }
+    
+    @PostMapping("/my-endpoint")
+    public void postMessage(@RequestBody String message) {
+        service.saveMessage(message);
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+    
+}
