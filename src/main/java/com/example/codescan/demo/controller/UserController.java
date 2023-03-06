50d3703626b972db1e75ca656c9c729e8c489d49
vhdeshpande <vaibhavihemantdeshpande@gmail.com>
2023-03-06 09:51:41 -0500

    Update UserRepository

--- src/main/java/com/example/codescan/demo/controller/UserController.java
+++ src/main/java/com/example/codescan/demo/controller/UserController.java
@@ -4,6 +4,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private final UserRepository userRepository;
+
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<User> getUserById(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(user);
+    }
+
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
         User user = userService.findById(id);
