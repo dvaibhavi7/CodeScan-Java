3a560fbe46bc65e519b658315feb2dc68deb7ef5
vhdeshpande <vaibhavihemantdeshpande@gmail.com>
2023-03-05 23:53:31 -0500

    US1000 - Initial Application Setup

---
+++ src/main/java/com/example/codescan/demo/CodescanApplication.java
@@ -0,0 +1,13 @@
+package com.example.codescan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodescanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodescanApplication.class, args);
+	}
+
+}
