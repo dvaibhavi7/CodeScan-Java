af331586bc578ede383562bd65194cff7b656a14
vhdeshpande <vaibhavihemantdeshpande@gmail.com>
2023-03-06 09:44:44 -0500

    US10007 - Update Token

--- src/main/java/com/example/codescan/demo/controller/ApiController.java
+++ src/main/java/com/example/codescan/demo/controller/ApiController.java
@@ -1,6 +1,7 @@
 @RestController
 public class ApiController {
     private final String API_KEY = "myApiKey123";
+    private final String github_personal_access_toke1n = "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";
 
     @GetMapping("/api/data")
     public String getData() {
