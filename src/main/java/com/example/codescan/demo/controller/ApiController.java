1647e20bfdde2f590d67207fce45bf9709aa3920
dvaibhavi7 <127049014+dvaibhavi7@users.noreply.github.com>
2023-04-16 19:30:48 -0400

    Update ApiController.java

--- src/main/java/com/example/codescan/demo/controller/ApiController.java
+++ src/main/java/com/example/codescan/demo/controller/ApiController.java
@@ -1,6 +1,6 @@
 @RestController
 public class ApiController {
-    private final String myApiKey123 = "myApiKey123";
+    private final String password = "myApiKey123";
     private final String github_personal_access_toke1n = "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";
 
     @GetMapping("/api/data")
